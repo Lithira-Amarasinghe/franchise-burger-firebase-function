e8bfe70347d6ebed46f918e85e57e67c414924da
Lithira Amarasinghe <amarasinghelithira@gmail.com>
2023-08-31 23:57:24 +0530

    code changes in functions

--- functions/src/modules/order/placeOrder.ts
+++ functions/src/modules/order/placeOrder.ts
@@ -8,13 +8,12 @@ export const placeOrder = functions.firestore.document("carts/{uid}")
     try {
       const newData = change.after.data();
       const previousData = change.before.data();
-      if (!previousData.createdAt && newData.createdAt) {
+      if (!previousData.status && newData.status=="ORDER_PLACED") {
         const data = newData;
         const date = new Date();
         console.log(data);
         const orderId = (+date.getTime()) * 2 + 500000;
         data.orderId = orderId;
-        data.status = "ORDER_PLACED";
         data.createdAt = date.toISOString();
         console.log(data);
         return admin.firestore().collection("orders").
@@ -22,7 +21,10 @@ export const placeOrder = functions.firestore.document("carts/{uid}")
           .then((result)=>{
             console.log(result);
             console.log("Field added:", newData.yourField);
-            admin.firestore().collection("carts").doc(uid).delete();
+            admin.firestore().collection("carts").
+              doc(uid).delete().then((del)=>{
+                console.log(del);
+              });
           })
           .catch((error)=>{
             console.log("Error in placing order: ", error);
