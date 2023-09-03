543b6ee8dad19d26d40a431e7b00c7ba8d434d32
Lithira Amarasinghe <amarasinghelithira@gmail.com>
2023-09-03 20:13:18 +0530

    AssignRoles function implemented. New set of roles can be added

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,10 +7,12 @@ const placeOrder = require("./modules/order/placeOrder");
 const orderReady = require("./modules/order/orderReady")
 const getAllUsers = require('./modules/user/GetAllUsers')
 const assignARole = require('./modules/user/AssignARole')
+const assignRoles = require('./modules/user/AssignRoles')
 
-exports.placeOrder = placeOrder.placeOrder;
-exports.orderReady = orderReady.orderReady;
-
-exports.assignUserRoleOnSign = assignUserRoleOnSignUp.assignUserRoleOnSignUp;
-exports.getAllUsers = getAllUsers.getAllUsers
-exports.assignARole = assignARole.assignARole;
+// exports.placeOrder = placeOrder.placeOrder;
+// exports.orderReady = orderReady.orderReady;
+//
+// exports.assignUserRoleOnSign = assignUserRoleOnSignUp.assignUserRoleOnSignUp;
+// exports.getAllUsers = getAllUsers.getAllUsers
+// exports.assignARole = assignARole.assignARole;
+exports.assignRoles = assignRoles.assignRoles;
--- functions/src/modules/user/AssignRoles.ts
+++ functions/src/modules/user/AssignRoles.ts
@@ -5,22 +5,16 @@ import * as corsModule from 'cors'
 
 const cors = corsModule({origin: true}); // Import the cors module
 
-export const assignARole = functions.https.onRequest((request, response) => {
+export const assignRoles = functions.https.onRequest((request, response) => {
     cors(request, response, async () => {
         let uid;
-        let roles:string[] = [];
+        let roles;
         try {
             const data:any = request?.body
             uid = data?.uid
-            roles.push(data.role);
+            roles = data.roles;
             console.log('received data : ', data)
-            await admin.auth().getUser(uid).then(result => {
-                result?.customClaims?.roles.forEach(role => {
-                    roles.push(role);
-                });
-            });
             // Add custom role to the user's custom claims
-            roles = Array.from(new Set(roles));
             console.log('Roles to assign : ', roles);
             const customClaims =  {roles: roles};
             await admin.auth().setCustomUserClaims(uid, customClaims).then(result=>{
